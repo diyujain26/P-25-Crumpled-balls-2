4be44d615284c1ae441470898541a9a3f76812e2
diyujain26 <67596731+diyujain26@users.noreply.github.com>
2020-08-17 05:09:13 -0700

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -3,15 +3,19 @@ class Ball {
     var options = {
         isStatic:false,
         restitution:0.3,
-        friction:0.4,
+        friction:0.5,
         density:1.4
   
     }
-    this.image = loadImage("sprites/Paperball.png")
     this.x= x;
     this.y= y;
     this.radius=radius;
+    this.image=loadImage("Paperball.png")
 
+    //this.body = Bodies.rectangle(x, y, width , height,{isStatic:true} );
+
+    this.body = Bodies.circle(this.x,this.y,this.radius,options);
+    
     World.add(world, this.body);
   }
   display(){
@@ -20,8 +24,13 @@ class Ball {
     push();
     translate(pos.x, pos.y);
     rotate(angle);
-    imageMode(CENTER);
-    image(this.image, 0, 0, this.width, this.height);
+    //fill("purple");
+    //strokeWeight(3);
+    //stroke("purple");
+    //ellipseMode(RADIUS);
+    //ellipse(0,0,this.radius);
+    ImageMode(CENTER);
+    image(this.image,0,0,this.width,this.height);
     pop();
-  } 
+  }
 };
